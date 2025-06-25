acc7ec62af98f53d5ae6b64545fa6643806a9dea
Mehdi Bahreini Nejad <mehdi.bahreyni1993@gmail.com>
2025-06-25 09:09:39 +0200

    Initial Commit

---
+++ VideoFeed/VideoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFeedApp.swift
+//  VideoFeed
+//
+//  Created by Mehdi Bahreini Nejad on 25.06.25.
+//
+
+import SwiftUI
+
+@main
+struct VideoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
